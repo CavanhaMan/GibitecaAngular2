9d9944bd9e0ccd2e9903a4da871c900142e5f9b3
CavanhaMan <cavanhaman@hotmail.com>
2019-05-05 23:28:49 -0300

    Adicionando rotas

---
+++ CadastroCli/src/app/app-router.ts
@@ -0,0 +1,40 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LivrosComponent } from './livros/livros.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { EditoraComponent } from './editora/editora.component';
+import { GibiComponent } from './gibi/gibi.component';
+import { ClienteComponent } from './cliente/cliente.component';
+
+const routesapp : Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'livros', component: LivrosComponent},
+  { path: 'categoria',component: CategoriaComponent},
+  { path: 'editora', component: EditoraComponent},
+  { path: 'gibi', component: GibiComponent},
+  { path: 'cliente', component: ClienteComponent}
+]
+export const Routing = RouterModule.forRoot(routesapp);
+/*
+import {Routes, RouterModule } from '@angular/router'
+import { ProfessorComponent } from './professor/professor.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+import { DisciplinaComponent } from './disciplina/disciplina.component';
+import { AreaConhecimentoComponent } from './area-conhecimento/area-conhecimento.component';
+import { HomeComponent } from "./home/home.component";
+
+const routes : Routes = [
+    {path: '',component: HomeComponent},
+    {path: './home/home.component.html',component: HomeComponent},
+    {path: 'professor',component: ProfessorComponent},
+    {path: './professor/professor.component.html',component: ProfessorComponent},
+    {path: 'conteudo',component: ConteudoComponent},
+    {path: './conteudo/conteudo.component.html',component: ConteudoComponent},
+    {path: 'disciplina',component: DisciplinaComponent},
+    {path: './disciplina/disciplina.component.html',component: DisciplinaComponent},
+    {path: 'areaConhecimento',component: AreaConhecimentoComponent},
+    {path: './area-conhecimento/area-conhecimento.component.html',component: AreaConhecimentoComponent}
+];
+export const RoutingModule = RouterModule.forRoot(routes);
+*/
--- CadastroCli/src/app/app.module.ts
+++ CadastroCli/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routing } from './app-router';
 /*--------------imports copiado do menu---------------*/
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,6 +19,7 @@ import { GibiComponent } from './gibi/gibi.component';
 import { CategoriaComponent } from './categoria/categoria.component';
 import { ClienteComponent } from './cliente/cliente.component';
 import { EditoraComponent } from './editora/editora.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -26,11 +28,13 @@ import { EditoraComponent } from './editora/editora.component';
     GibiComponent,
     CategoriaComponent,
     ClienteComponent,
-    EditoraComponent
+    EditoraComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    Routing,
 /*--------------imports copiado do menu---------------*/
     BrowserAnimationsModule,
     MatIconModule,
