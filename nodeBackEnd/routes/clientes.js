f006fbb1b555fe41336f8a88bf2c9fd7cae192f5
CavanhaMan <cavanhaman@hotmail.com>
2019-07-06 18:48:41 -0300

    Deletar o recurso e testando com o Postman

--- nodeBackEnd/routes/clientes.js
+++ nodeBackEnd/routes/clientes.js
@@ -33,4 +33,14 @@ router.get('/search/params',(req,res) => {
         .catch(err => console.log(err));
 });
 
+router.delete("/:id", (req,res) => {
+    Cliente.destroy({
+        where: {codigo: req.params.id}
+    })
+    .then(result => res.sendStatus(204))
+    .catch(error => {
+        res.status(412).json({msg:error.message});
+    });
+});
+
 module.exports = router;
