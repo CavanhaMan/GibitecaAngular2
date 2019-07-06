19ad613f82b998cfe9dc7e2c0f1413ccb20548b1
CavanhaMan <cavanhaman@hotmail.com>
2019-07-06 16:23:49 -0300

    Adicionando recurso estático no app.js pag18

--- nodeBackEnd/app.js
+++ nodeBackEnd/app.js
@@ -6,4 +6,13 @@ const app = express();
 
 app.get("/", (req,res) => res.json({status: "Nodejs backend"}));
 
+app.get("/clientes", (req,res) => {
+    res.json([
+                {'codigo':1,'nome':'Cavanha'},
+                {'codigo':2,'nome':'Bolsonaro'}
+            ]
+    )
+});
+
+// app.listen(PORT, () => console.log("escutando na porta "+PORTA));
 app.listen(PORT, () => console.log("escutando na porta "+PORT));
